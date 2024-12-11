018f36487df10e40f26c450b7b9870bd9902e50a
Alejandro Estornell <aaesalamanca@gmail.com>
2024-12-11 11:52:07 +0100

    feat: Calculate the total check & per person based on the tip percentage (#1)
    * feat: Initial form layout for splitting the check
    
    * feat: Type the check amount
    
    * feat: Pick the number of people to split the check
    
    * feat: Pick the tip percentage
    
    * feat: Calculate & show the total check amount
    
    * feat: Calculate & show the check amount per person

--- WeSplitHWS/ContentView.swift
+++ WeSplitHWS/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @FocusState private var checkAmountIsFocused: Bool
+
+    @State private var numberOfPeopleIndex = 0
+
+    @State private var tipPercentage = 20
+
+    private var totalAmount: Double {
+        checkAmount + checkAmount * Double(tipPercentage) / 100
+    }
+
+    private var amountPerPerson: Double {
+        totalAmount / Double(numberOfPeopleIndex + 2)
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Check details") {
+                    TextField(
+                        "Check amount", value: $checkAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD")
+                    )
+                    .focused($checkAmountIsFocused)
+                    .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeopleIndex)
+                    {
+                        ForEach(2..<16) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+
+                Section("How much tip dou you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            if $0.isMultiple(of: 5) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                }
+
+                Section("Total amount") {
+                    Text(
+                        totalAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"))
+                }
+
+                Section("Amount per person") {
+                    Text(
+                        amountPerPerson,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplitHWS")
+            .toolbar {
+                if checkAmountIsFocused {
+                    Button("Done") {
+                        checkAmountIsFocused.toggle()
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
